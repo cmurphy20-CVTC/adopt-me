174dc40d9be119126e7ffaa5f2e02a292f2bce38
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-08-03 21:30:58 -0500

    React Router added

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,25 +1,24 @@
 import Pet from "./Pet";
 
-const Results = ({pets}) => {
+const Results = ({ pets }) => {
   return (
     <div className="search">
       {!pets.length ? (
         <h1>No Pets Found</h1>
       ) : (
         pets.map((pet) => {
-        
-        return (
-          
-      <Pet 
-           name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          images={pet.breed}
-          location={`${pet.city}, ${pet.state}`}
-          key={pet.id}
-      />
-        );
-      })
+          return (
+            <Pet
+              animal={pet.animal}
+              id={pet.id}
+              name={pet.name}
+              breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
+              key={pet.id}
+            />
+          );
+        })
       )}
     </div>
   );
