36a74246da6bda24344e2f1a37e7ad5a1f3a96b3
Casey <89794911+cmurphy20-CVTC@users.noreply.github.com>
2023-08-06 22:31:20 -0500

    styling added to forms

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -21,8 +21,10 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg 
+      bg-gray-200 p-10 shadow-lg"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -41,12 +43,19 @@ const SearchParams = () => {
         ) : null}
         <label htmlFor="location">
           Location
-          <input name="lcoation" id="location" placeholder="Location" />
+          <input
+            className="mb-5 block w-60 disabled:opacity-50"
+            type="text"
+            name="lcoation"
+            id="location"
+            placeholder="Location"
+          />
         </label>
 
         <label htmlFor="animal">
           Animal
           <select
+            className="mb-5 block w-60"
             id="animal"
             value={animal}
             onChange={(e) => {
@@ -67,7 +76,12 @@ const SearchParams = () => {
 
         <label htmlFor="breed">
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            className="mb-5 block w-60"
+            disabled={!breeds.length}
+            id="breed"
+            name="breed"
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -77,7 +91,7 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <button>Submit</button>
+        <button className="rounded px-6 py-2 text-white hover:opacity-50 border-none bg-orange-500">Submit</button>
       </form>
       <Results pets={pets} />
     </div>
